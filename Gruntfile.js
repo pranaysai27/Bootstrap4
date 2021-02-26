24bb5c9cc7215b30e65af393afd9d5ab247af734
Pranay Sai Garepally <pranaysai27@gmail.com>
2021-02-26 11:57:33 +0530

    Gulp Part 2

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,81 +1,8 @@
-'use script';
+'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+  // Define the configuration for all the tasks
+  grunt.initConfig({
 
-	require('time-grunt')(grunt);
-
-	require('jit-grunt')(grunt);
-	grunt.initConfig({
-		sass: {
-			dist: {
-				files: {
-					'css/styles.css' : 'css/styles.scss'
-				}
-			}
-		},
-		watch: {
-			files: 'css/*.scss',
-			tasks: ['sass']
-		},
-		browserSync: {
-			dev: {
-				bsFiles: {
-                    src : [
-                        'css/*.css',
-                        '*.html',
-                        'js/*.js'
-                    ]
-                },
-                options: {
-                    watchTask: true,
-                    server: {
-                        baseDir: "./"
-                    }
-                }
-			}
-		},
-		copy: {
-			html: {
-				files:[{
-					expand: true,
-					dot: true,
-					cwd: './',
-					src: ['*.html'],
-					dest: 'dist'
-				}]
-			},
-			fonts: {
-				files: [{
-					expand: true,
-					dot: true,
-					cwd: 'node_modules/font-awesome',
-                    src: ['fonts/*.*'],
-                    dest: 'dist'
-				}]
-			}
-		},
-		clean: {
-            build: {
-                src: [ 'dist/']
-            }
-        },
-        imagemin: {
-            dynamic: {
-                files: [{
-                    expand: true,                  // Enable dynamic expansion
-                    cwd: './',                   // Src matches are relative to this path
-                    src: ['img/*.{png,jpg,gif}'],   // Actual patterns to match
-                    dest: 'dist/'                  // Destination path prefix
-                }]
-            }
-        }
-	});
-
-	grunt.registerTask('css',['sass']);
-	grunt.registerTask('default',['browserSync','watch']);
-	grunt.registerTask('build', [
-        'clean',
-        'copy',
-        'imagemin'
-    ]);
+  });
 };
